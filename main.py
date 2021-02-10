dad38edc8ac8b61d1c3f90142b14304ee9ac73fd
Kronifer <44979306+Kronifer@users.noreply.github.com>
2021-02-09 18:42:45 -0600

    Work on configuration

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+#! /usr/bin/python3.7
+
 import kvJSON as kv
+import os
+
+def init():
+    kv.init("data.json")
+    try:
+        kv.getData("gamelist")
+    except:
+        config = input("It seems you have not configured TRM yet. Would you like to now? If no, TRM will be autoconfigured. [y/n] ")
+        if config == "y":
+            os.system("clear")
+            print("First, you need to decide what games you want users to play.")
+
