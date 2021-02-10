1f4c77beb700718b04faf031844234608718192c
Kronifer <dillonr5@live.wsd1.org>
2021-02-10 16:05:18 +0000

    more config stuff

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ def init():
         config = input("It seems you have not configured TRM yet. Would you like to now? If no, TRM will be autoconfigured. [y/n] ")
         if config == "y":
             os.system("clear")
-            print("First, you need to decide what games you want users to play.")
-
+            print("First, you need to decide what games you want users to play.\n")
+            print("""Here is a list of current supported games:
+            Cataclysm: Dark Days Ahead,
+            Angband
+            Unangband
+            Dungeon Crawl: Stone Soup
+            Nethack\n
+            """)
